fb3884f77b0a36da80ca443cbee646449632f653
Misato-331 <misato33100901@yahoo.co.jp>
2020-01-13 14:33:56 +0900

    test

---
+++ CalculatorAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalculatorAPP
+//
+//  Created by 川中美里 on 2020/01/13.
+//  Copyright © 2020 川中美里. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
